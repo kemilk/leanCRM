a7e3a6751328e681afcbf234cd9772ad9388ece2
Franz Bernhardt <Franzbernhardt@gmail.com>
2021-05-24 17:48:14 +0200

    Opportunity begonnen

--- src/main/java/com/ironhack/leancrm/Opportunity.java
+++ src/main/java/com/ironhack/leancrm/Opportunity.java
@@ -8,6 +8,51 @@ public class Opportunity {
     private Product product;
 
 
+    // Constructer
 
+    public Opportunity(Integer id, Integer quantity, Contact decisionMaker, Status status, Product product) {
+        setId(id);
+        setQuantity(quantity);
+        setDecisionMaker(decisionMaker);
+        setStatus(status);
+        setProduct(product);
+    }
+
+    // Getter and Setter
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Lead lead) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Contact getDecisionMaker() {
+        return decisionMaker;
+    }
+    public void setDecisionMaker(Contact decisionMaker) {
+        this.decisionMaker = decisionMaker;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
 
