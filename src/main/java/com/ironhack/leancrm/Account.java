5482a39156d7659675b6d10f0b522687a773b550
PatDue <mail@patrickduepmann.de>
2021-05-25 14:20:37 +0200

    Adding Constructor and Getters & Setters to Account Class

--- src/main/java/com/ironhack/leancrm/Account.java
+++ src/main/java/com/ironhack/leancrm/Account.java
@@ -1,6 +1,6 @@
 package com.ironhack.leancrm;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Account {
     private Integer id;
@@ -9,7 +9,99 @@ public class Account {
     private String companyName;
     private String city;
     private String country;
-    public static HashMap<Integer, Contact> contactList;
-    private HashMap<Integer, Opportunity> opportunityList;
+    private HashSet<Contact> contactSet;
+    private HashSet<Opportunity> opportunitySet;
 
+    private static HashSet<Account> accountSet;
+
+    public static void addAccount(Integer id, Account account) {
+        accountSet.add(account);
+    }
+
+    public static HashSet<Account> getAccountSet() {
+        return accountSet;
+    }
+
+    public Account(Integer id, Industry industry, Integer employeeCount, String companyName, String city, String country, Contact contact, Opportunity opportunity) {
+        this.id = id;
+        this.industry = industry;
+        this.employeeCount = employeeCount;
+        this.companyName = companyName;
+        this.city = city;
+        this.addContact(contact);
+        this.addOpportunity(opportunity);
+        addAccount(id, this);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Industry getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(Industry industry) {
+        this.industry = industry;
+    }
+
+    public Integer getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public void setEmployeeCount(Integer employeeCount) {
+        this.employeeCount = employeeCount;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public HashSet<Contact> getContactSet() {
+        return contactSet;
+    }
+
+    public void listContacts() {
+        for (Contact selected : this.contactSet) {
+            System.out.println(selected);
+        }
+    }
+
+    public void addContact(Contact contact) {
+        this.contactSet.add(contact);
+    }
+
+    public HashSet<Opportunity> getOpportunitySet() {
+        return opportunitySet;
+    }
+
+    public void listOpportunities() {
+        for (Opportunity selected : this.opportunitySet) {
+            System.out.println(selected);
+        }
+    }
+
+    public void addOpportunity(Opportunity opportunity) {
+        this.opportunitySet.add(opportunity);
+    }
 }
